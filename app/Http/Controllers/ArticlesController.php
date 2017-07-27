9544c2348801e3ab8f423eec5c9ceffdb4bf7835
Sid <sam810929@gmail.com>
2017-07-27 22:02:17 +0800

    補剛剛沒捕到的

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -18,13 +18,16 @@ class ArticlesController extends Controller
 		// 從資料庫撈回 articles table 的所有資料，作為變數 articles 的值
 		$articles = Articles::get();
 
+		// 回傳 view : task/index.blade.php，並回以上的變數們
 		return view('articles.index', compact('id','name', 'articles'));
 	}
     
-    public function show($id)
+    public function show($id) // 接收從routes/web.php 設定的 id
     {
+    	//建立一個變數，值是依照 id，到 articles table 抓回該id 的資料
     	$articles = Articles::find($id);
 
+    	// 回傳 view ：task/show.blade.php，並帶回 task 變數
     	return view('articles.show')->with(compact('articles'));
     }
 }
